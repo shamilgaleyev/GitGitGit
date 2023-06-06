e2743b0ff7bf99b38f5903dcaed0a4fd79fa0df8
shamilgaleyev <galeyevshamil@gmail.com>
2023-06-06 16:22:22 -0400

    3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 7; i++) {
             System.out.println("Hello world!");
-        }
 
+        }
     }
 }
